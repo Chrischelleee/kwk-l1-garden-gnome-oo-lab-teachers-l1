fc6109d6de029d67b74c7c582d92eb34c0a2766f
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-26 19:37:02 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -25,10 +25,7 @@ class GardenGnome
     return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
   end
 end
-gnome1 = GardenGnome.name("Walter the Worst")
-gnome2 = GardenGnome.name("James the Jeerk")
-gnome2 = GardenGnome.age("3421")
-puts gnome1 
+ 
 
 gnome1.name = "Walter the Worst"
 
